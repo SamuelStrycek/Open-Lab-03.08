113291b7eb0f76931b320e9902a05e62d26dde89
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-20 23:33:22 +0200

    Add files via upload

---
+++ Checker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_03._08
+{
+    public class Checker
+    {
+        public bool IsPlural(string word)
+        {
+          if (word.EndsWith("s"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
